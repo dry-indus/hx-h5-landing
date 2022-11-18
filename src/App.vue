836a4f8e32472322c7be578c8bb1f689873659ea
dry-indus <dry_indus@163.com>
2022-11-19 00:37:37 +0700

    编译

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,24 @@
 <template>
   <Suspense>
     <template #default>
-      <!-- <router-view v-slot="{ Component, route }">
-      </router-view> -->
+      <router-view v-slot="{ Component, route }">
+        <PageSlide :transition="route.meta.transition">
+          <!-- <div> -->
+          <keep-alive>
+            <component
+              :is="Component"
+              v-if="route.meta && route.meta.keepAlive"
+              :key="route.meta.usePathKey ? route.fullPath : undefined"
+            />
+          </keep-alive>
+          <component
+            :is="Component"
+            v-if="!(route.meta && route.meta.keepAlive)"
+            :key="route.meta.usePathKey ? route.fullPath : undefined"
+          />
+          <!-- </div> -->
+        </PageSlide>
+      </router-view>
     </template>
     <template #fallback> Loading... </template>
   </Suspense>
