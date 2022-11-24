d1a78b2490e9bdfef0aafceac9577f86f7749b20
chenjiaoyue <chenjiaoyue@innovsharing.com>
2022-11-24 22:31:08 +0800

    feat: add logo

--- src/views/store/index.vue
+++ src/views/store/index.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="storeWarpper">
     <div class="header">
-      <LandingLogo />
+      <div class="logoWarrper">
+        <img class="logo" src="../../assets/images/logo.png" alt="" />
+        <img v-for="(item,index) in state.logoList" :key="index" :class="['stars', 'l' + (index + 1), 'anima']" :src="item" alt="" />
+      </div>
       <p class="appName">海选新鲜</p>
     </div>
     <main>
@@ -23,98 +26,243 @@
 </template>
 
 <script lang="ts" setup name="StorePage">
-  import { reactive } from 'vue';
-  // import LandingLogo from '../../components/logo/index.vue';
-  import { storeSearch } from '/@/api/landing';
-  const formData = reactive({
-    search: '',
-  });
-  const state = reactive<any>({
-    storeList: [],
-  });
-  const handleChange = () => {
-    search(formData.search);
+import { reactive } from 'vue';
+import { storeSearch } from '/@/api/landing';
+import xing from '../../assets/images/xing.png'
+import shell from '../../assets/images/shell.png'
+const formData = reactive({
+  search: '',
+});
+const state = reactive<any>({
+  storeList: [],
+  logoList: [],
+});
+const handleChange = () => {
+  search(formData.search);
+};
+const getRandomNode = (arr, count) => {
+  var shuffled = arr.slice(0),
+    i = arr.length,
+    min = i - count,
+    temp,
+    index;
+  while (i-- > min) {
+    index = Math.floor((i + 1) * Math.random());
+    temp = shuffled[index];
+    shuffled[index] = shuffled[i];
+    shuffled[i] = temp;
+  }
+  return shuffled.slice(min).map(m=> m.prtrait);
+}
+const randomOrder = (arr) => {
+  var length = arr.length,
+    randomIndex,
+    temp;
+  while (length) {
+    randomIndex = Math.floor(Math.random() * (length--));
+    temp = arr[randomIndex];
+    arr[randomIndex] = arr[length];
+    arr[length] = temp
+  }
+  return arr;
+}
+const search = (keywords) => {
+  const params = {
+    keywords,
+    pageNumber: 1,
+    pageSize: 20,
   };
-  const search = (keywords) => {
-    const params = {
-      keywords,
-      pageNumber: 1,
-      pageSize: 20,
-    };
 
-    storeSearch(params).then((res) => {
-      if (res?.status === 200) {
-        state.storeList = res.data.data.result;
-        console.log(state.storeList, res.data);
-      }
-    });
-  };
-  search('string');
+  storeSearch(params).then((res) => {
+    if (res?.status === 200) {
+      const result = res.data.data.result;
+      state.storeList = result
+      const list = JSON.parse(JSON.stringify(getRandomNode(result, 5)))
+      const starArr = [xing,xing,xing]
+      const shellArr = [shell,shell,shell]
+      const all = [...list,...starArr,...shellArr]
+      state.logoList = randomOrder(all)
+      console.log(JSON.parse(JSON.stringify(list)),all)
+    }
+  });
+};
+search('string');
 </script>
 
 <style lang="scss" scoped>
-  .storeWarpper {
-    height: 100vh;
-    width: 100vw;
-    background: #000;
+.logoWarrper {
+  width: 100%;
+  height: 300px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transform-style: preserve-3d;
+  perspective-origin: center;
+  perspective: 80px;
+}
+.logo {
+  width: 218px;
+  height: 233px;
+  img {
+    width: 100%;
+    height: 100%;
   }
-  .header {
-    height: 40%;
-    position: relative;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    .appName {
-      font-size: 36px;
-      color: #ffffff;
-    }
+}
+.stars {
+  width: 70px;
+  height: 70px;
+  animation: anima;
+  opacity: 0;
+  border-radius: 50%;
+}
+.l1 {
+  position: absolute;
+  right: -19px;
+  animation-delay: 1s;
+}
+.l2 {
+  position: absolute;
+  right: -31px;
+  top: -10px;
+  animation-delay: 2s;
+}
+.l3 {
+  position: absolute;
+  right: -31px;
+  bottom: -10px;
+  animation-delay: 3s;
+}
+.l4 {
+  position: absolute;
+  left: -19px;
+  animation-delay: 4s;
+}
+.l5 {
+  position: absolute;
+  left: -31px;
+  top: -10px;
+  animation-delay: 5s;
+}
+.l6 {
+  position: absolute;
+  left: -31px;
+  bottom: -10px;
+  animation-delay: 6s;
+}
+.l7{
+  position: absolute;
+    left: 41px;
+    top: -75px;
+    animation-delay: 7s;
+}
+.l8{
+  position: absolute;
+    left: 200px;
+    top: -75px;
+    animation-delay:8s;
+}
+.l9{
+  position: absolute;
+    left: 400px;
+    top: -75px;
+    animation-delay: 9s;
+}
+.l10{
+  position: absolute;
+    left: 600px;
+    top: -75px;
+    animation-delay: 10s;
+}
+.l11{
+  position: absolute;
+    left: 100px;
+    bottom: -60px;
+    animation-delay: 11s;
+}
+.l12{
+  position: absolute;
+    right: 100px;
+    bottom: -60px;
+    animation-delay: 12s;
+}
+@keyframes move {
+  0% {
+    transform: translateZ(-120px);
+    opacity: 1;
   }
-  .storeList {
-    width: 570px;
-    height: 600px;
-    display: flex;
-    flex-direction: column;
-    box-sizing: border-box;
+  100% {
+    transform: translateZ(120px);
   }
-  .item {
-    height: 100px;
-    border-bottom: 1px solid #888888;
-    display: flex;
+}
+.anima {
+  animation-name: move;
+  animation-timing-function: linear;
+  /* animation-delay: 2s; */
+  animation-iteration-count: infinite;
+  animation-duration: 4s;
+}
+.storeWarpper {
+  height: 100vh;
+  width: 100vw;
+  background: #000;
+}
+.header {
+  height: 40%;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  .appName {
+    font-size: 36px;
     color: #ffffff;
-    align-items: center;
-    justify-content: space-between;
-    padding: 0 2vw;
-    .info,
-    .data {
-      display: flex;
-      align-items: center;
-    }
-    img {
-      width: 42px;
-      height: 50px;
-      margin-right: 10px;
-    }
-    .storeTitle {
-      font-size: 3.6vw;
-    }
   }
-  main {
-    height: 60%;
-    background: #000;
+}
+.storeList {
+  width: 570px;
+  height: 600px;
+  display: flex;
+  flex-direction: column;
+  box-sizing: border-box;
+}
+.item {
+  height: 100px;
+  border-bottom: 1px solid #888888;
+  display: flex;
+  color: #ffffff;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 2vw;
+  .info,
+  .data {
     display: flex;
-    flex-direction: column;
-    justify-content: center;
     align-items: center;
   }
-  .nut-input {
-    width: 570px;
-    height: 78px;
-    background: #383838;
-    border-bottom: none;
-    padding-left: 2.5vw;
+  img {
+    width: 42px;
+    height: 50px;
+    margin-right: 10px;
   }
-  .nut-icon-search {
-    color: #888888;
+  .storeTitle {
+    font-size: 3.6vw;
   }
+}
+main {
+  height: 60%;
+  background: #000;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.nut-input {
+  width: 570px;
+  height: 78px;
+  background: #383838;
+  border-bottom: none;
+  padding-left: 2.5vw;
+}
+.nut-icon-search {
+  color: #888888;
+}
 </style>
--- src/views/store/list.vue
+++ src/views/store/list.vue
@@ -1,11 +1,26 @@
 <template>
   <div class="storeWarpper">
     <div class="header">
-      <LandingLogo />
+      <div class="logoWarrper">
+        <img class="logo" src="../../assets/images/logo.png" alt="" />
+        <img class="stars l1 anima" src="../../assets/images/xing.png" alt="" />
+        <img class="stars l2 anima" src="../../assets/images/xing.png" alt="" />
+        <img class="stars l3 anima" src="../../assets/images/xing.png" alt="" />
+        <img class="stars l4 anima" src="../../assets/images/xing.png" alt="" />
+        <img class="stars l5 anima" src="../../assets/images/xing.png" alt="" />
+        <img class="stars l6 anima" src="../../assets/images/xing.png" alt="" />
+      </div>
       <p class="appName">海选新鲜</p>
     </div>
     <main>
-      <div class="store" v-for="(item,index) in state.info" :key="index" :style="{background: item.backgroundRPGA}" @click="toPage(item.url)">{{ item.name }}</div>
+      <div
+        class="store"
+        v-for="(item, index) in state.info"
+        :key="index"
+        :style="{ background: item.backgroundRPGA }"
+        @click="toPage(item.url)"
+        >{{ item.name }}</div
+      >
     </main>
   </div>
 </template>
@@ -19,12 +34,12 @@ const state = reactive<any>({
   info: [],
 });
 const toPage = (url) => {
-  window.location.href = url
-}
+  window.location.href = url;
+};
 const setting = () => {
   storeSetting().then((res) => {
     if (res?.status === 200) {
-      state.info = res.data.data.entry  ;
+      state.info = res.data.data.entry;
       console.log(state.info, res.data);
     }
   });
@@ -33,6 +48,80 @@ setting();
 </script>
 
 <style lang="scss" scoped>
+.logoWarrper{
+  width: 100%;
+  height: 300px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transform-style: preserve-3d;
+  perspective-origin: center;
+  perspective: 80px;
+}
+.logo {
+  width: 218px;
+  height: 233px;
+  img {
+    width: 100%;
+    height: 100%;
+  }
+}
+.stars {
+  width: 70px;
+  height: 70px;
+  animation: anima;
+  opacity: 0;
+}
+.l1 {
+  position: absolute;
+  right: -19px;
+  animation-delay: 1s;
+}
+.l2 {
+  position: absolute;
+  right: -31px;
+  top: -10px;
+  animation-delay: 2s;
+}
+.l3 {
+  position: absolute;
+  right: -31px;
+  bottom: -10px;
+  animation-delay: 3s;
+}
+.l4 {
+  position: absolute;
+  left: -19px;
+  animation-delay: 4s;
+}
+.l5 {
+  position: absolute;
+  left: -31px;
+  top: -10px;
+  animation-delay: 5s;
+}
+.l6{
+  position: absolute;
+  left: -31px;
+  bottom: -10px;
+  animation-delay: 6s;
+}
+@keyframes move {
+  0% {
+    transform: translateZ(-120px);
+    opacity: 1;
+  }
+  100% {
+    transform: translateZ(60px);
+  }
+}
+.anima {
+  animation-name: move;
+  animation-timing-function: linear;
+  /* animation-delay: 2s; */
+  animation-iteration-count: infinite;
+  animation-duration: 4s;
+}
 .storeWarpper {
   height: 100vh;
   width: 100vw;
